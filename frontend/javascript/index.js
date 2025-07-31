332a70cc972ce8254bf40306c95d8fc2fd1e72c1
Miguel <miguel.torres@hey.com>
2025-07-31 16:45:53 +0200

    Add Catpuccin color theme to code snippets & syntax highlighting

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,7 +1,12 @@
 import "$styles/index.css"
-import "$styles/syntax-highlighting.css"
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import ruby from 'highlight.js/lib/languages/ruby';
+
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('ruby', ruby);
+hljs.highlightAll();
 
 // Import all JavaScript & CSS files from src/_components
 import components from "$components/**/*.{js,jsx,js.rb,css}"
 
-console.info("Bridgetown is loaded!")
